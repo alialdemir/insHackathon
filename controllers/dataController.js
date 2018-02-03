b2337262363f8a23f32f51286fde1707d630d69b
alialdemir <aldemirali93@gmail.com>
2018-02-03 17:06:38 +0300

    title

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -2,11 +2,6 @@ var fs = require('fs');
 var path = require("path");
 
 function get(req, res) {
-    let css = req.query.css;
-    let js = req.query.js;
-    let rule = req.query.rule;
-    let config = req.query.config;
-
     fs.readFile('data/data.json', 'utf8', (err, data) => {
         if (err) return res.status(500).send({ message: 'Dosya kaydedilemedi.' });
 
@@ -18,17 +13,18 @@ function get(req, res) {
 
 function post(req, res) {
     let css = req.body.css;
+    let title = req.body.title;
     let js = req.body.js;
-    let rule = req.body.rule;
     let config = req.body.config;
-
-    let obj = { css, js, rule, config };
 
 
     fs.readFile('data/data.json', 'utf8', (err, data) => {
         if (err) return res.status(500).send({ message: 'Dosya kaydedilemedi.' });
 
-        var arrData = JSON.parse(data || "[]");
+        var arrData = JSON.parse(data || []);
+        let id = arrData.length > 0 ? arrData[arrData.length - 1].id + 1 : 1;
+
+        let obj = { id, css, js, config };
         arrData.push(obj);
 
         fs.writeFile('data/data.json', JSON.stringify(arrData), 'utf8', (err, data) => {
@@ -43,8 +39,19 @@ function template(req, res) {
     return res.sendFile('template.html', { root: path.join(__dirname, '../views') });
 }
 
+function deleteItem(req, res) {
+    let id = req.body.id;
+    fs.readFile('data/data.json', 'utf8', (err, data) => {
+        if (err) return res.status(500).send({ message: 'Dosya kaydedilemedi.' });
+
+        let arrData = JSON.parse(data);
+
+    });
+}
+
 module.exports = {
     get,
     post,
-    template
+    template,
+    deleteItem
 }
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const api = express.Router();
 api.get('/data', dataController.get);
 api.post('/data', dataController.post);
 api.get('/template', dataController.template);
+api.delete('/template', dataController.deleteItem);
 
 module.exports = api;
